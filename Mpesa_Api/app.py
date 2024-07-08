cf56797166a0176fe0fca6327a1ac25d5f6f08ba
winniegithubrit <winnie.jomo@student.moringaschool.com>
2024-07-08 19:16:11 +0300

    :art: generating qrcode

--- Mpesa_Api/app.py
+++ Mpesa_Api/app.py
@@ -1,9 +1,15 @@
 import requests
 from requests.auth import HTTPBasicAuth
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 import time
+from models import db, QRCode
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///daraja.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 
 consumer_key = "DKmoDUw4M4Ynk6RyFB0SkIxseZNdOXV4sSmhoFAEgi88kvYv"
 consumer_secret = "b3lv2UvzmQDF7Pq9Bl85rAjGXcbV4GpBchi2Fok5REcQ8BUMAoxk1TeVnjYjbDYt"
@@ -28,5 +34,41 @@ def get_token():
     token, expires_in = get_access_token()
     return jsonify({"access_token": token, "expires_in": expires_in})
 
+# generating qrcode
+@app.route('/generate_qrcode', methods=['POST'])
+def generate_qrcode():
+    token, _ = get_access_token()
+    headers = {
+        "Authorization": f"Bearer {token}",
+        "Content-Type": "application/json"
+    }
+    payload = request.json
+    print("Request Payload:", payload)
+    url = "https://sandbox.safaricom.co.ke/mpesa/qrcode/v1/generate"
+    response = requests.post(url, headers=headers, json=payload)
+    print("Response Status Code:", response.status_code)
+    print("Response Headers:", response.headers)
+    print("Response Body:", response.text)
+
+    if response.status_code == 200:
+        # Storing the request in the database
+        qr_request = QRCode(
+            MerchantName=payload.get('MerchantName'),
+            RefNo=payload.get('RefNo'),
+            Amount=payload.get('Amount'),
+            TrxCode=payload.get('TrxCode'),
+            CPI=payload.get('CPI'),
+            Size=payload.get('Size')
+        )
+        db.session.add(qr_request)
+        db.session.commit()
+        return jsonify(qr_request.to_dict()), 200
+    else:
+        return jsonify(response.json()), response.status_code
+
+
+
 if __name__ == '__main__':
+  with app.app_context():
+    db.create_all()
     app.run(debug=True)
---
+++ Mpesa_Api/models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class QRCode(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    MerchantName = db.Column(db.String(300), nullable=False)
+    RefNo = db.Column(db.String(100), nullable=False)
+    Amount = db.Column(db.String(100), nullable=False)
+    TrxCode = db.Column(db.String(100), nullable=False)
+    CPI = db.Column(db.String(100), nullable=False)
+    Size = db.Column(db.String(100), nullable=False)
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "MerchantName": self.MerchantName,
+            "RefNo": self.RefNo,
+            "Amount": self.Amount,
+            "TrxCode": self.TrxCode,
+            "CPI": self.CPI,
+            "Size": self.Size
+        }
